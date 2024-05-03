b93b467a3f2dc6bd1a726806879661ed3a362553
uytyks <uytyks@udel.edu>
2024-05-03 15:51:35 -0400

    position and world bounds added

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-let map = `AAAAAAAAAA<br>
-AABAAAAAAA<br>
-AAAAAADAAA<br>
-AAAACAAAAA<br>
-AAAAAEAAAA<br>
-FAAAAAAAAA<br>`;
+let map = `AAAAAAAAAAA<br>
+AABAAAAAAAA<br>
+AAAAAADAAAA<br>
+AAAACAAAAAA<br>
+AAAAAEAAAAA<br>
+FAAAAAAAAAA<br>
+AAAAAAAAGAA<br>`;
 
 //The holy array that holds the entire map
+//Will eventually just hold a viewport
 mapArray = [];
-
 mapRow = [];
+position = [0,0];
+bounds = [-5,5,-3,3]
+
+//The below function just converts the block of text into a 2D array
 for(i = 0; i < map.length;i++){
     if(map.substring(i,i+4) == "<br>"){
         i = i+4
@@ -26,21 +31,35 @@ for(i = 0; i < map.length;i++){
 document.onkeydown = checkMovement;
 
 function checkMovement(e) {
-
     if (e.keyCode == '38') {
         // up arrow
-        moveUp();
+        if(position[1] < bounds[3]){
+            position[1]++;
+        }
     }
     else if (e.keyCode == '40') {
         // down arrow
+        if(position[1] > bounds[2]){
+            position[1]--;
+        }
     }
     else if (e.keyCode == '37') {
        // left arrow
+       if(position[0] > bounds[0]){
+        position[0]--;
+        }
     }
     else if (e.keyCode == '39') {
        // right arrow
+       if(position[0] < bounds[1]){
+        position[0]++;
+        }
     }
+    console.log(position);
 
+    //Every time that a movement key is pressed, write to the screen
+    //Keep track of a movement variable
+    //Draw the viewport around that variable
     let mapString = "";
     for(j = 0; j < mapArray.length;j++){
         for(k = 0; k < mapArray[j].length;k++){
